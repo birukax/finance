37b565490dd4342230cc116990a6feed3e5a31c2
birukax <birukax@gmail.com>
2025-06-05 10:05:27 +0300

    backend - finished working on the url for the GL app

--- api/gl/urls.py
+++ api/gl/urls.py
@@ -1,20 +1,25 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 from . import views
 
-app_name = "gl"
+router = routers.DefaultRouter()
+
+
+router.register(
+    r"proration-types",
+    views.ProrationTypeViewSet,
+    basename="proration-type",
+)
+router.register(
+    r"accounts",
+    views.AccountViewSet,
+    basename="account",
+)
+router.register(
+    r"net-changes",
+    views.NetChangeViewSet,
+)
 
 urlpatterns = [
-    path("account/list", views.account_list, name="account_list"),
-    path("account/edit/<int:id>", views.edit_account, name="edit_account"),
-    path("proration-type/list", views.proration_type_list, name="proration_type_list"),
-    path(
-        "proration-type/create",
-        views.create_proration_type,
-        name="create_proration_type",
-    ),
-    path(
-        "proration-type/edit/<int:id>",
-        views.edit_proration_type,
-        name="edit_proration_type",
-    ),
+    path("", include(router.urls)),
 ]
